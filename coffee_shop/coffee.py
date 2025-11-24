2577f4fccdd2ff84e9f79124ce7e1d7acc31f066
joshwizard <murikij@gmail.com>
2025-11-24 09:05:53 +0300

    added code to coffee and customer

--- coffee_shop/coffee.py
+++ coffee_shop/coffee.py
@@ -1,2 +1,17 @@
 class Coffee:
-    pass
+    def __init__(self, name):
+        self.name = name
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        if not isinstance (value, str):
+            raise TypeError("Text must be of string type")
+        
+        if len(value) < 3:
+            print("Coffee name must have at least 3 characters")
+        
+        self._name = value
--- coffee_shop/customer.py
+++ coffee_shop/customer.py
@@ -1,2 +1,17 @@
 class Customer:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            raise TypeError ("Name must be a string")
+        
+        if 1 <= len(value) >= 15:
+            raise ValueError ("Name must have a value between 1 and 15 characters")
+            
+        self._name = value
