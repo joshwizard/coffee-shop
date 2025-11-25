fe792bd12aeccc5192214a3e5da8f454cbbb668c
joshwizard <murikij@gmail.com>
2025-11-25 12:37:31 +0300

    Added code to coffee

--- coffee_shop/coffee.py
+++ coffee_shop/coffee.py
@@ -15,3 +15,19 @@ class Coffee:
             print("Coffee name must have at least 3 characters")
         
         self._name = value
+
+    def orders(self):
+        from order import Order
+        return [order for order in Order.all if order.coffee == self]
+
+    def customers(self):
+        return list(set(order.customer for order in self.orders()))
+
+    def num_orders(self):
+        return len(self.orders())
+
+    def average_price(self):
+        orders = self.orders()
+        if not orders:
+            return 0
+        return sum(order.price for order in orders) / len(orders)
