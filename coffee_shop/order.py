7b1da299b6c7f0bcd086b48930848bb5e24d3cb4
joshwizard <murikij@gmail.com>
2025-11-25 16:00:56 +0300

    Added Order.py code

--- coffee_shop/order.py
+++ coffee_shop/order.py
@@ -1,3 +1,39 @@
+from customer import Customer
+from coffee import Coffee
 
 class Order:
-    pass
+    def __init__(self, customer, coffee, price):
+        self.customer = customer
+        self.coffee = coffee
+        self.price = price
+
+    @property
+    def customer(self):
+        return self._customer
+    
+    @customer.setter
+    def customer(self, value):
+        if isinstance(value, Customer):
+            raise ValueError("Customer must be an instance of Customer class")
+        self._customer = value
+    
+    @property
+    def coffee(self):
+        return self._coffee
+    
+    @coffee.setter
+    def coffee(self, value):
+        if isinstance(value, Coffee):
+            raise ValueError("Coffee must be an instance of Coffee class")
+        self._coffee = value
+
+    @property
+    def price(self):
+        return self._price
+    
+    @price.setter
+    def price(self, value):
+        if not isinstance(value, (int, float)) or not (1.0 <= value <= 10):
+            raise ValueError("Price must be a number between 1.0 and 10.0")
+        self._price = float(value)
+
